a5773ee991e29c61ff3c4e11fb4450f37cf205df
M0CH4771 <sengoku.akb@gmail.com>
2026-09-19 22:31:50 +0900

    Connect Ticket Management to the deployed Google Apps Script backend

--- docs/config.js
+++ docs/config.js
@@ -1,5 +1,5 @@
 // Set once after deploying Code.gs + Bridge.html as a GAS web app.
 // This URL is a public endpoint, not a secret or a member key.
 window.LIVE_POCKET_CONFIG = {
-  gasUrl: ""
+  gasUrl: "https://script.google.com/macros/s/AKfycbxFGR-XUhTCvw5_1ne8k7GaJAHo377I27u3_Qs2ZkgiQSphEVIR1oPeEhxKlf7cCXA0/exec"
 };
