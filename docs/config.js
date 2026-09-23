832f55ba5fb08618910ac75ff4c0c3c170f37ab5
M0CH4771 <sengoku.akb@gmail.com>
2026-09-23 13:32:51 +0900

    Switch to the new Google Apps Script deployment

--- docs/config.js
+++ docs/config.js
@@ -1,5 +1,5 @@
 // Set once after deploying Code.gs + Bridge.html as a GAS web app.
 // This URL is a public endpoint, not a secret or a member key.
 window.LIVE_POCKET_CONFIG = {
-  gasUrl: "https://script.google.com/macros/s/AKfycbxFGR-XUhTCvw5_1ne8k7GaJAHo377I27u3_Qs2ZkgiQSphEVIR1oPeEhxKlf7cCXA0/exec"
+  gasUrl: "https://script.google.com/macros/s/AKfycbxglI_Lghp_uAON09cAvxAaLcfr2N4OytJra5L7nRikMEXloF6C5k77_1JHPWx7x8MUTQ/exec"
 };
